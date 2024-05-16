d80bca05e30ed258bef4968b656b392552544317
avila-r <avila.dev@outlook.com>
2024-05-16 14:51:46 -0300

    feat: add actuator dependency

---
+++ src/main/java/com/avila/zecompany/model/GeoData.java
@@ -0,0 +1,49 @@
+package com.avila.zecompany.model;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import java.util.List;
+
+@NoArgsConstructor @AllArgsConstructor @Builder
+@Entity @Table(name = "geo_data")
+public class Geography {
+
+    @Converter @Component
+    private record DataConverter(@Autowired ObjectMapper objectMapper) implements AttributeConverter<List<?>, String> {
+        @Override @SneakyThrows
+        public String convertToDatabaseColumn(List<?> attribute) {
+            return objectMapper.writeValueAsString(attribute);
+        }
+
+        @Override @SneakyThrows
+        public List<?> convertToEntityAttribute(String data) {
+            return objectMapper.readValue(data, objectMapper.getTypeFactory().constructCollectionType(List.class, Object.class));
+        }
+    }
+
+    @Getter public enum GeoType {
+        ADDRESS("Point"), AREA("MultiPolygon");
+        private final String type;
+        GeoType(String type) {
+            this.type = type;
+        }
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "partner_id")
+    private Partner partner;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "type")
+    private GeoType type;
+
+    @Convert(converter = DataConverter.class)
+    @Column(name = "coordinates", columnDefinition = "jsonb")
+    private List<?> coordinates;
+}
