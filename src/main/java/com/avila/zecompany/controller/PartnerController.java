72102c9aa17952ba7f25fc77defd765e6c2ef5d3
avila-r <avila.dev@outlook.com>
2024-05-17 17:38:26 -0300

    feat: delegate tasks for services

--- src/main/java/com/avila/zecompany/controller/PartnerController.java
+++ src/main/java/com/avila/zecompany/controller/PartnerController.java
@@ -1,11 +1,7 @@
 package com.avila.zecompany.controller;
-import com.avila.zecompany.dto.GeoDataDTO;
 import com.avila.zecompany.dto.PartnerRequestDTO;
 import com.avila.zecompany.dto.PartnerResponseDTO;
-import com.avila.zecompany.model.GeoData;
-import com.avila.zecompany.model.Partner;
-import com.avila.zecompany.repository.GeoDataRepository;
-import com.avila.zecompany.repository.PartnerRepository;
+import com.avila.zecompany.service.PartnerService;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -17,63 +13,17 @@ import java.util.List;
 @RequestMapping("/partner")
 @AllArgsConstructor
 public class PartnerController {
-    private final PartnerRepository partnerRepository;
-    private final GeoDataRepository geoDataRepository;
+    private final PartnerService service;
 
     @GetMapping
     public ResponseEntity<List<PartnerResponseDTO>> listAll(){
-        return ResponseEntity.ok(partnerRepository.findAll().stream()
-                        .map(partner -> PartnerResponseDTO.builder()
-                                .id(partner.getId())
-                                .tradingName(partner.getTradingName())
-                                .ownerName(partner.getOwnerName())
-                                .document(partner.getDocument())
-                                .coverageArea(GeoDataDTO.builder()
-                                        .type(geoDataRepository.findByPartnerId(partner.getId()).orElseThrow().getType().equals(GeoData.GeoType.ADDRESS) ? "Point" : "MultiPolygon")
-                                        .coordinates(geoDataRepository.findByPartnerId(partner.getId()).orElseThrow().getCoordinates())
-                                        .build())
-                                .build())
-                        .toList()
-                );
+        return ResponseEntity.ok(service.listAllPartners());
     }
 
     @PostMapping
     @Transactional public ResponseEntity<PartnerResponseDTO> insert(@RequestBody PartnerRequestDTO request){
-        Partner partner = Partner.builder()
-                .document(request.document())
-                .tradingName(request.tradingName())
-                .ownerName(request.ownerName())
-                .build();
-        GeoData area = GeoData.builder()
-                .partner(partner)
-                .type(request.coverageArea().type().equals("Point") ? GeoData.GeoType.ADDRESS : GeoData.GeoType.AREA)
-                .coordinates(request.coverageArea().coordinates())
-                .build();
-        GeoData address = GeoData.builder()
-                .partner(partner)
-                .type(request.address().type().equals("Point") ? GeoData.GeoType.ADDRESS : GeoData.GeoType.AREA)
-                .coordinates(request.address().coordinates())
-                .build();
-        partnerRepository.save(partner);
-        geoDataRepository.save(area);
-        geoDataRepository.save(address);
         return ResponseEntity
                 .status(HttpStatus.CREATED)
-                .body(
-                        PartnerResponseDTO.builder()
-                                .id(partnerRepository.findByDocument(partner.getDocument()).orElseThrow().getId())
-                                .tradingName(partner.getTradingName())
-                                .ownerName(partner.getOwnerName())
-                                .document(partner.getDocument())
-                                .coverageArea(GeoDataDTO.builder()
-                                        .type(area.getType().equals(GeoData.GeoType.ADDRESS) ? "Point" : "MultiPolygon")
-                                        .coordinates(area.getCoordinates())
-                                        .build())
-                                .address(GeoDataDTO.builder()
-                                        .type(address.getType().equals(GeoData.GeoType.ADDRESS) ? "Point" : "MultiPolygon")
-                                        .coordinates(area.getCoordinates())
-                                        .build())
-                                .build()
-                );
+                .body(service.insertNewPartner(request));
     }
 }
