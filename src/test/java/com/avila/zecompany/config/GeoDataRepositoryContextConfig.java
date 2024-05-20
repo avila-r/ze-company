ae7be836cd5de4dcfe61f65a69da38cbd41f7c82
avila-r <avila.dev@outlook.com>
2024-05-20 19:30:34 -0300

    feat: publish object mapper bean

---
+++ src/test/java/com/avila/zecompany/config/GeoDataRepositoryContextConfig.java
@@ -0,0 +1,12 @@
+package com.avila.zecompany.config;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GeoDataRepositoryContextConfig {
+    @Bean
+    public ObjectMapper objectMapper(){
+        return new ObjectMapper();
+    }
+}
